d02bd3186b87c059fa6c1aee97262657e9744c55
s-Aura-v <slamich2@oswego.edu>
2024-09-18 23:06:05 -0400

    Attempted addToFloorPlan with different sizes
    TODO:
    Catch exceptions for when it goes over or under the array index.
    
    There's something off with the main ==null code because it won't allow the code to enter the  if statements that looks at their volume.
    
    Might have to archive it for now

--- src/main/java/Facility.java
+++ src/main/java/Facility.java
@@ -3,7 +3,6 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Facility is a class that holds a 2d array, representing a floor plan. Each array index represents a location on the floor.
- *
  */
 public class Facility extends Thread {
 
@@ -35,13 +34,10 @@ public class Facility extends Thread {
      */
     public void randomizePeopleInFacility() {
         // TODO: Figure out how to determine 12 without using 12 explicitly
-        int floorMaxCapacity = 12;
 
         int index = 0;
         while (index < listOfPeople.length) {
-            int xValue = ThreadLocalRandom.current().nextInt(floorMaxCapacity);
-            int yValue = ThreadLocalRandom.current().nextInt(floorMaxCapacity);
-            addToFloorPlan(xValue, yValue, index);
+            addToFloorPlan(index);
             index++;
         }
         this.solutionSet.add(floorPlan);
@@ -49,26 +45,89 @@ public class Facility extends Thread {
 
     /**
      * Synchronized are locks used for atomicity. It locks the method then unlocks it at the end.
-     * @param xValue: the x-axis in the floor plan
-     * @param yValue: the y-axis in the floor plan
+     * xValue: the x-axis in the floor plan
+     * yValue: the y-axis in the floor plan
      * @param index: the index for the Person stored in the Person array
      * @return void: adds a person to the floor plan
      */
-    private synchronized void addToFloorPlan(int xValue, int yValue, int index) {
-        floorPlan[xValue][yValue] = listOfPeople[index];
+//    private synchronized void addToFloorPlan(int index) {
+//        int xValue = ThreadLocalRandom.current().nextInt(FACILITY_DIMENSION);
+//        int yValue = ThreadLocalRandom.current().nextInt(FACILITY_DIMENSION);
+//        if (floorPlan[xValue][yValue] != null) {
+//            floorPlan[xValue][yValue] = listOfPeople[index];
+//        } else {
+//            addToFloorPlan(index);
+//        }
+//    }
+
+    /**
+     * Attempt 2: Try to take care of the space.
+     */
+    private synchronized void addToFloorPlan(int index) {
+        int xValue = ThreadLocalRandom.current().nextInt(FACILITY_DIMENSION);
+        int yValue = ThreadLocalRandom.current().nextInt(FACILITY_DIMENSION);
+        Person person = listOfPeople[index];
+        int personVolume = listOfPeople[index].function;
+        if (floorPlan[xValue][yValue] == null) {
+            if (personVolume == 0) {
+                floorPlan[xValue][yValue] = listOfPeople[index];
+            } else if (personVolume == 1) {
+                boolean added = false;
+                if (floorPlan[xValue - 1][yValue] != null) {
+                    floorPlan[xValue][yValue] = person;
+                    floorPlan[xValue - 1][yValue] = person;
+                    added = true;
+                }
+                if (floorPlan[xValue + 1][yValue] != null && (!added)) {
+                    floorPlan[xValue][yValue] = person;
+                    floorPlan[xValue + 1][yValue] = person;
+                }
+            } else if (personVolume == 2) {
+                boolean added = false;
+                if (floorPlan[xValue][yValue - 1] != null) {
+                    floorPlan[xValue][yValue] = person;
+                    floorPlan[xValue][yValue - 1] = person;
+                    added = true;
+                }
+                if (floorPlan[xValue][yValue + 1] != null && (!added)) {
+                    floorPlan[xValue][yValue] = person;
+                    floorPlan[xValue][yValue + 1] = person;
+                }
+            } else if (personVolume == 3) {
+                if (floorPlan[xValue + 1][yValue] != null && floorPlan[xValue - 1][yValue] != null &&
+                        floorPlan[xValue][yValue - 1] != null && floorPlan[xValue][yValue + 1] != null &&
+                        floorPlan[xValue - 1][yValue + 1] != null && floorPlan[xValue - 1][yValue - 1] != null &&
+                        floorPlan[xValue + 1][yValue + 1] != null && floorPlan[xValue + 1][yValue - 1] != null) {
+                    floorPlan[xValue][yValue] = person;
+                    floorPlan[xValue + 1][yValue] = person;
+                    floorPlan[xValue - 1][yValue] = person;
+                    floorPlan[xValue][yValue + 1] = person;
+                    floorPlan[xValue][yValue - 1] = person;
+                    floorPlan[xValue + 1][yValue + 1] = person;
+                    floorPlan[xValue + 1][yValue - 1] = person;
+                    floorPlan[xValue - 1][yValue + 1] = person;
+                    floorPlan[xValue - 1][yValue - 1] = person;
+                }
+            }
+        } else {
+            addToFloorPlan(index);
+        }
     }
 
     private void addToSolutionSet() {
 
     }
+
+    /**
+     * Functions are defined as 0,1,2,3,4 representing "Doctor", "Assistant", "Student", "Patient", and "Dean Of Medicine."
+     * [x] -> [y] implies x likes y
+     * [x] -/> [y] implies x does not like y
+     * There is a max radius of [z]. The closer to z, the less value the likability has.
+     * Affinity = distance * likeability
+     */
     private void calculateAffinity() {
 
     }
-
-
-
-
-
 
 
 }
